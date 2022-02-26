1f1c953f17b9a961c45b75461dcd168f4c045952
Jay Parikh <jay@iaminworld.com>
2022-02-26 00:10:34 -0500

    Implemented normalized correlated coefficient for recognition

--- task1.py
+++ task1.py
@@ -11,7 +11,6 @@ Please follow the guidelines mentioned in the project1.pdf
 Do NOT import any library (function, module, etc.).
 """
 
-
 import argparse
 import json
 import os
@@ -19,7 +18,7 @@ import glob
 import cv2
 import numpy as np
 
-#TODO
+# TODO
 large_width = 400
 np.set_printoptions(linewidth=large_width)
 
@@ -34,6 +33,7 @@ def read_image(img_path, show=False):
 
     return img
 
+
 def show_image(img, delay=1000):
     """Shows an image.
     """
@@ -42,9 +42,10 @@ def show_image(img, delay=1000):
     cv2.waitKey(delay)
     cv2.destroyAllWindows()
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="cse 473/573 project 1.")
-    #TODO
+    # TODO
     parser.add_argument(
         "--test_img", type=str, default="./data/test_img.jpg",
         help="path to the image used for character detection (do not change this arg)")
@@ -56,6 +57,7 @@ def parse_args():
         help="directory to which results are saved (do not change this arg)")
     args = parser.parse_args()
     return args
+
 
 def ocr(test_img, characters):
     """Step 1 : Enroll a set of characters. Also, you may store features in an intermediate file.
@@ -79,19 +81,20 @@ def ocr(test_img, characters):
         
     """
     # TODO Add your code here. Do not modify the return and input arguments
-    
-    #TODO
-    #enrollment()
+
+    # TODO
+    # enrollment()
 
     json_map = detection(test_img)
-    
+
     recognition(test_img, json_map, characters)
-    
-    #with open("results.json", "w") as write_file:
+
+    # with open("results.json", "w") as write_file:
     #    json.dump(json_map, write_file)
-    
-    #TODO
-    #raise NotImplementedError
+
+    # TODO
+    # raise NotImplementedError
+
 
 def enrollment():
     """ Args:
@@ -100,8 +103,9 @@ def enrollment():
     You are free to decide the return.
     """
     # TODO: Step 1 : Your Enrollment code should go here.
-    #TODO
-    #raise NotImplementedError
+    # TODO
+    # raise NotImplementedError
+
 
 def detection(data):
     """ 
@@ -112,14 +116,14 @@ def detection(data):
     You are free to decide the return.
     """
     # TODO: Step 2 : Your Detection code should go here.
-    
+
     bw_threshold = 100
 
-    #image = np.array(data[54:108, 4:92])
+    # image = np.array(data[54:108, 4:92])
     image = np.array(data)
     image = np.where(image <= bw_threshold, 0, 255)
-    #print(image)
-    #show_image(image, delay=2000)
+    # print(image)
+    # show_image(image, delay=2000)
     '''image = np.array([
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
@@ -134,70 +138,68 @@ def detection(data):
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
     image[image == 0] = 255'''
     link_map = []
-    labels = np.zeros((image.shape), dtype = np.uint64)
+    labels = np.zeros((image.shape), dtype=np.uint64)
     label_counter = 1
-    #First Pass
-    
+    # First Pass
+
     for row in range(image.shape[0]):
-        for col in range(image.shape[1]):  
-            if (image[row][col] < 255): 
-                neighbors = np.zeros((2,), dtype=np.uint64) # west is 0, north is 1
-                if (col-1 >= 0):
-                    if (image[row][col-1] == 0 and labels[row][col-1] != 0): #if neighbor has same value as current and has label
-                        neighbors[0] = labels[row][col-1] #add west label
-                if (row-1 >= 0):
-                    if (image[row-1][col] == 0 and labels[row-1][col] != 0):
-                        neighbors[1] = labels[row-1][col] #add north label
-                
-                if (neighbors == np.zeros((2,))).all(): #no neighbors
+        for col in range(image.shape[1]):
+            if (image[row][col] < 255):
+                neighbors = np.zeros((2,), dtype=np.uint64)  # west is 0, north is 1
+                if (col - 1 >= 0):
+                    if (image[row][col - 1] == 0 and labels[row][col - 1] != 0):  # if neighbor has same value as current and has label
+                        neighbors[0] = labels[row][col - 1]  # add west label
+                if (row - 1 >= 0):
+                    if (image[row - 1][col] == 0 and labels[row - 1][col] != 0):
+                        neighbors[1] = labels[row - 1][col]  # add north label
+
+                if (neighbors == np.zeros((2,))).all():  # no neighbors
                     labels[row][col] = label_counter
                     label_counter += 1
                 else:
-                    if (neighbors == np.zeros((2,))).any(): #any one neighbor has no label
+                    if (neighbors == np.zeros((2,))).any():  # any one neighbor has no label
                         labels[row][col] = np.max(neighbors)
                     else:
                         labels[row][col] = np.min(neighbors)
-                        if (neighbors[0] != neighbors[1]):  #both neighbors are not same, but are linked
+                        if (neighbors[0] != neighbors[1]):  # both neighbors are not same, but are linked
                             link_map.append((neighbors[0], neighbors[1]))
-    
-    #print(f"linked map: {set(link_map)}")
+
+    # print(f"linked map: {set(link_map)}")
     linked = [i for i in range(label_counter)]
     for i, j in set(link_map):
         union(linked, i, j)
-    
+
     for row in range(image.shape[0]):
         for col in range(image.shape[1]):
             if (image[row][col] < 255):
                 labels[row][col] = find(linked, labels[row][col])
-      
+
     json_map = []
     color = (0, 0, 0)
     for label in np.unique(labels):
-        #print(np.where(labels == label)[0])
+        # print(np.where(labels == label)[0])
         if label != 0:
-            #print("For label:", label)
+            # print("For label:", label)
             temp = np.where(labels == label)
-            #cv2.rectangle(data, (np.min(temp[1]), np.min(temp[0])), (np.max(temp[1]), np.max(temp[0])), color, 1)
-            #bbox.append((np.min(a[0]), np.max(a[0]), np.min(a[1]), np.max(a[1])))
+            # cv2.rectangle(data, (np.min(temp[1]), np.min(temp[0])), (np.max(temp[1]), np.max(temp[0])), color, 1)
+            # bbox.append((np.min(a[0]), np.max(a[0]), np.min(a[1]), np.max(a[1])))
             x = int(np.min(temp[1]))
             y = int(np.min(temp[0]))
             w = int(np.max(temp[1]) - np.min(temp[1]))
             h = int(np.max(temp[0]) - np.min(temp[0]))
             json_map.append({"bbox": [x, y, w, h], "name": ""})
-    #num = 0
-    #print(labels[bbox[num][0]:bbox[num][1], bbox[num][2]:bbox[num][3]]*255)
-    #show_image(data, delay=10000)
-    
-    
-    
-    #print("counter: ", label_counter)
-    #print("map: ", linked)
-    
-    #print(len(np.unique(labels)))
-    #print(np.unique(labels))
-    #show_image(labels, delay=5000)
-    #TODO
-    #raise NotImplementedError
+    # num = 0
+    # print(labels[bbox[num][0]:bbox[num][1], bbox[num][2]:bbox[num][3]]*255)
+    # show_image(data, delay=10000)
+
+    # print("counter: ", label_counter)
+    # print("map: ", linked)
+
+    # print(len(np.unique(labels)))
+    # print(np.unique(labels))
+    # show_image(labels, delay=5000)
+    # TODO
+    # raise NotImplementedError
     return json_map
 
 
@@ -205,14 +207,17 @@ def find(data, i):
     if i != data[i]:
         data[i] = find(data, data[i])
     return data[i]
-    
+
+
 def union(data, i, j):
     pi, pj = find(data, i), find(data, j)
     if pi != pj:
         data[pi] = pj
-        
+
+
 def connected(data, i, j):
     return find(data, i) == find(data, j)
+
 
 def recognition(data, json_map, characters):
     """ 
@@ -222,23 +227,126 @@ def recognition(data, json_map, characters):
     You are free to decide the return.
     """
     # TODO: Step 3 : Your Recognition code should go here.
-    
-    '''for i, item in enumerate(json_map):
+    # x, y, w, h = json_map[84]["bbox"]
+    # print(data[y:y + h, x:x + w])
+    # return
+
+    # Test Image Stats
+    # line 1 - 28
+    # line 2 - 27
+    # line 3 - 23
+    # line 4 - 20
+    # line 5 - 24
+    # line 6 - 21
+    # Total: 143
+
+    # For 2 best 75 with BW
+    # 4
+    # 80
+    # 123
+    # 124
+
+    # For a best 65 with BW
+    # 13
+    # 16
+    # 38
+    # 48
+    # 73
+    # 75*
+    # 112
+    # 140
+    # 141
+
+    # For dot best 70-90 with BW, i-53, 54, 95, 139
+    # 53
+    # 54
+    # 95
+    # 139
+
+    # For e best 73 with BW
+    # 20
+    # 26
+    # 39
+    # 40
+    # 49
+    # 50
+    # 52
+    # 77
+    # 94
+    # 107
+    # 109
+    # 113
+    # 117
+    # 118
+
+    # For c best 90 with BW
+    # 24
+    # 69
+    # 89
+    # 105
+
+    corrs = []
+    # print(f"for char: {characters[2][0]}")
+    # print(characters[2][1][2:-2, 1:-3])
+    for i, item in enumerate(json_map):
         x, y, w, h = item["bbox"]
-        print(f"{i}:")
-        print(data[y:y+h, x:x+w])'''
-    #2: index 4
-    #a: index 13
-    x, y, w, h = json_map[4]["bbox"]
-    #print(data[y:y+h, x:x+w])
-    #characters[0][1][1:-1, 4:-5] #2
-    #characters[1][1][1:-1, 2:-4] #a
-    template = np.array(characters[0][1][1:-1, 4:-5])
-    image = np.array(data[y:y+h, x:x+w])
-    '''a = (a - np.mean(a)) / (np.std(a) * len(a))
-    b = (b - np.mean(b)) / (np.std(b))
-    c = np.correlate(a, b, 'full')
-    print(c)'''
+        # if(i == 141):
+        # print(data[y:y+h, x:x+w])
+        # print(data[y:y+h, x:x+w])
+        # 2: index 4
+        # a: index 13
+        # x, y, w, h = json_map[4]["bbox"]
+        # print(data[y:y+h, x:x+w])
+        # characters[0][1][1:-1, 4:-5] #2
+        # characters[1][1][1:-1, 2:-4] #a
+        # characters[2][1][2:-2, 1:-3] #c
+        # characters[3][1][3:-2, 2:-5] #dot
+        # characters[4][1][1:-3,1:-4] #e
+        template = np.array(characters[2][1][2:-2, 1:-3])
+        image = np.array(data[y:y + h, x:x + w])
+        # image = np.where(image <= 100, 0, 255)
+        template = np.where(template <= 100, 0, 255)
+        # get the maximum of both sizes
+        max_size = (np.max([template.shape[0], image.shape[0]]), np.max([template.shape[1], image.shape[1]]))
+        # maps the smaller image onto bigger
+        centered_image = np.ones(max_size) * 255
+        centered_template = np.ones(max_size) * 255
+        image_min = np.subtract(max_size, image.shape) // 2
+        image_max = np.add(max_size, image.shape) // 2
+        template_min = np.subtract(max_size, template.shape) // 2
+        template_max = np.add(max_size, template.shape) // 2
+        # sets the values
+        centered_image[image_min[0]: image_max[0], image_min[1]: image_max[1]] = image
+        centered_template[template_min[0]:template_max[0], template_min[1]:template_max[1]] = template
+        # if(i == 53 or i == 54 or i == 95 or i == 143):
+        #     print(centered_image)
+        #     print("||||")
+        #     print(centered_template)
+        # releases unused memory
+        del template, image
+
+        # norm_a = np.linalg.norm(centered_image)
+        # centered_image = centered_image / norm_a
+        # norm_b = np.linalg.norm(centered_template)
+        # centered_template = centered_template / norm_b
+        image_mean = np.mean(centered_image)
+        template_mean = np.mean(centered_template)
+        image_std = np.std(centered_image)
+        template_std = np.std(centered_template)
+
+        summation = np.sum(np.multiply((centered_image - image_mean), (centered_template - template_mean)))
+        if summation == 0 and (image_std == 0 or template_std == 0):
+            corr = 1000
+        else:
+            corr = np.divide(summation, (np.std(centered_image) * np.std(centered_template)))
+        corrs.append(corr)
+        # print(f"{i}: {corr}")
+    # mean_corr = np.mean(corrs)
+    max_corr = np.max(corrs) * 0.9
+    for i, corr in enumerate(corrs):
+        if (corr > max_corr):
+            print(i)
+
     '''for irow in range(image.shape[0]):
         for icol in range(image.shape[1]):
             CCorr = 0
@@ -246,10 +354,9 @@ def recognition(data, json_map, characters):
                 for tcol in range(template.shape[1]):
                     CCorr += abs(image[irow][icol]- template[trow][tcol])
     print(CCorr)'''
-                  
-    
-    #TODO
-    #raise NotImplementedError
+
+    # TODO
+    # raise NotImplementedError
 
 
 def save_results(coordinates, rs_directory):
@@ -262,25 +369,25 @@ def save_results(coordinates, rs_directory):
 
 
 def main():
-    #TODO
+    # TODO
     import sys
     np.set_printoptions(threshold=sys.maxsize)
-    
+
     args = parse_args()
-    
+
     characters = []
 
-    all_character_imgs = glob.glob(args.character_folder_path+ "/*")
-    
-    for each_character in all_character_imgs :
+    all_character_imgs = glob.glob(args.character_folder_path + "/*")
+
+    for each_character in all_character_imgs:
         character_name = "{}".format(os.path.split(each_character)[-1].split('.')[0])
         characters.append([character_name, read_image(each_character, show=False)])
 
     test_img = read_image(args.test_img)
-    
+
     results = ocr(test_img, characters)
-    #TODO
-    #save_results(results, args.rs_directory)
+    # TODO
+    # save_results(results, args.rs_directory)
 
 
 if __name__ == "__main__":
