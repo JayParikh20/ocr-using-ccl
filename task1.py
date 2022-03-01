c02fd649a7827a472e8bafc8fb5a1ac4715c447a
Jay Parikh <jay@iaminworld.com>
2022-02-28 20:03:17 -0500

    trimmed template white padding working on zoning

--- task1.py
+++ task1.py
@@ -83,43 +83,44 @@ def ocr(test_img, characters):
     # TODO Add your code here. Do not modify the return and input arguments
 
     # TODO
-    # enrollment()
+    template_kps = enrollment(characters)
     # show_ground(test_img)
     # return
-    json_map = detection(test_img)
 
-    results = recognition(test_img, json_map, characters)
+    json_map = detection(test_img)
+    # return
+    # FAST
+    # for i, item in enumerate(json_map):
+    #     x, y, w, h = item["bbox"]
+    #     img = np.array(test_img[y:y + h, x:x + w])
+    #     sift = cv2.SIFT_create()
+    #     kp = sift.detect(img, None)
+    #     img = cv2.drawKeypoints(img, kp, img)
+    #     cv2.imwrite(f'features/image_keypoints_{i}.jpg', img)
+    return
+    # FAST detection
+    image_kps = []
+    for i, item in enumerate(json_map):
+        x, y, w, h = item["bbox"]
+        img = np.array(test_img[y:y + h, x:x + w])
+        # img = np.pad(img, 2, 'constant', constant_values=(255))
+        # fast = cv2.FastFeatureDetector_create()
+        # kp: tuple = fast.detect(img)
+        # image_kps.append([key_point.pt for key_point in kp])
+        # img = cv2.drawKeypoints(img, kp, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imwrite(f'features/image_keypoints_{i}.jpg', img)
+
+    results = recognition(image_kps, template_kps, json_map, characters)
 
     # with open("results.json", "w") as write_file:
     #     json.dump(json_map, write_file)
-    return results
+    # return results
 
     # TODO
     # raise NotImplementedError
 
 
-# TODO
-def show_ground(data):
-    color = (0, 0, 0)
-    ground = np.array(data)
-    with open("data/groundtruth.json") as file:
-        groundtruth = json.load(file)
-
-    for j, item in enumerate(groundtruth):
-        x, y, w, h = item["bbox"]
-        cv2.rectangle(ground, (x, y), (x+w, y+h), color, 1)
-    show_image(ground, delay=10000)
-
-    res = np.array(data)
-    with open("results.json") as file:
-        results = json.load(file)
-
-    for j, item in enumerate(results):
-        x, y, w, h = item["bbox"]
-        cv2.rectangle(res, (x, y), (x+w, y+h), color, 1)
-    show_image(res, delay=10000)
-
-def enrollment():
+def enrollment(characters):
     """ Args:
         You are free to decide the input arguments.
     Returns:
@@ -127,6 +128,36 @@ def enrollment():
     """
     # TODO: Step 1 : Your Enrollment code should go here.
     # TODO
+
+    # SIFT
+    # for char in characters:
+    #     print("sift feature for:", char[0])
+    #     img = char[1]
+    #     sift = cv2.SIFT_create()
+    #     kp = sift.detect(img, None)
+    #     if(kp):
+    #         print(kp[0].pt)
+    #     img = cv2.drawKeypoints(img, kp, img)
+    #     cv2.imwrite(f'features/template_keypoints_{char[0]}.jpg', img)
+
+    # FAST detection
+    template_kps = []
+    for char in characters:
+        print("for feature:", char[0])
+        img: np.ndarray = char[1]
+        print("img shape:", img.shape)
+        # trimming white borders
+        del_cols = np.where(np.mean(img, axis=0) > 245)
+        img = np.delete(img, del_cols, axis=1)
+        del_rows = np.where(np.mean(img, axis=1) > 245)
+        img = np.delete(img, del_rows, axis=0)
+        # img = np.delete(img, np.where(np.mean(img, axis=1) > 250))
+        # fast = cv2.FastFeatureDetector_create()
+        # kp: tuple = fast.detect(img)
+        # template_kps.append([key_point.pt for key_point in kp])
+        # img = cv2.drawKeypoints(img, kp, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imwrite(f'features/template_keypoints_{char[0]}.jpg', img)
+    return template_kps
     # raise NotImplementedError
 
 
@@ -138,7 +169,18 @@ def detection(data):
     Returns:
     You are free to decide the return.
     """
+
     # TODO: Step 2 : Your Detection code should go here.
+
+    def search(tree, child):
+        if child != tree[child]:
+            tree[child] = search(tree, tree[child])
+        return tree[child]
+
+    def merge(tree, child, parent):
+        c, p = search(tree, child), search(tree, parent)
+        if p != c:
+            tree[c] = p
 
     bw_threshold = 100
 
@@ -190,15 +232,15 @@ def detection(data):
     # print(f"linked map: {set(link_map)}")
     linked = [i for i in range(label_counter)]
     for i, j in set(link_map):
-        union(linked, i, j)
+        merge(linked, i, j)
 
     for row in range(image.shape[0]):
         for col in range(image.shape[1]):
             if (image[row][col] < 255):
-                labels[row][col] = find(linked, labels[row][col])
+                labels[row][col] = search(linked, labels[row][col])
 
     json_map = []
-    color = (0, 0, 0)
+    # color = (0, 0, 0)
     for label in np.unique(labels):
         # print(np.where(labels == label)[0])
         if label != 0:
@@ -213,7 +255,7 @@ def detection(data):
             json_map.append({"bbox": [x, y, w, h], "name": "UNKNOWN"})
     # num = 0
     # print(labels[bbox[num][0]:bbox[num][1], bbox[num][2]:bbox[num][3]]*255)
-    # show_image(data, delay=10000)
+    # show_image(data, delay=5000)
 
     # print("counter: ", label_counter)
     # print("map: ", linked)
@@ -226,23 +268,7 @@ def detection(data):
     return json_map
 
 
-def find(data, i):
-    if i != data[i]:
-        data[i] = find(data, data[i])
-    return data[i]
-
-
-def union(data, i, j):
-    pi, pj = find(data, i), find(data, j)
-    if pi != pj:
-        data[pi] = pj
-
-
-def connected(data, i, j):
-    return find(data, i) == find(data, j)
-
-
-def recognition(data, json_map, characters):
+def recognition(image_kps: list, template_kps: list, json_map, characters):
     """ 
     Args:
         You are free to decide the input arguments.
@@ -255,136 +281,86 @@ def recognition(data, json_map, characters):
     # return
 
     # Test Image Stats
-    # line 1 - 28
-    # line 2 - 27
-    # line 3 - 23
-    # line 4 - 20
-    # line 5 - 24
-    # line 6 - 21
+    # line 1 - 28, line 2 - 27, line 3 - 23, line 4 - 20, line 5 - 24, line 6 - 21
     # Total: 143
 
     # For 2 best 75 with BW
-    # 4
-    # 80
-    # 123
-    # 124
+    # 4, 80, 123, 124
 
     # For a best 65 with BW
-    # 13
-    # 16
-    # 38
-    # 48
-    # 73
-    # 75*
-    # 112
-    # 140
-    # 141
+    # 13, 16, 38, 48, 73, 75*, 112, 140, 141
 
     # For dot best 70-90 with BW, i-53, 54, 95, 139
-    # 53
-    # 54
-    # 95
-    # 139
+    # 53, 54, 95, 139
 
     # For e best 73 with BW
-    # 20
-    # 26
-    # 39
-    # 40
-    # 49
-    # 50
-    # 52
-    # 77
-    # 94
-    # 107
-    # 109
-    # 113
-    # 117
-    # 118
+    # 20, 26, 39, 40, 49, 50, 52, 77, 94, 107, 109, 113, 117, 118
 
     # For c best .92 with BW
-    # 24
-    # 69
-    # 89
-    # 105
+    # 24, 69, 89, 105
+
     # ncc_map = []
     # print(f"for char: {characters[2][0]}")
 
-    for i, char in enumerate(characters):
-        # characters[0][1][1:-1, 4:-5] #2
-        # characters[1][1][1:-1, 2:-4] #a
-        # characters[2][1][2:-2, 1:-3] #c
-        # characters[3][1][3:-2, 2:-5] #dot
-        # characters[4][1][1:-3, 1:-4] #e
-        print(f"Loop for {char[0]}: ")
-        template = np.array(char[1])
-        # curr_ncc_map = []
-        for j, item in enumerate(json_map):
-            x, y, w, h = item["bbox"]
-            image = np.array(data[y:y + h, x:x + w])
-            # if(j == 141):
-            # print(data[y:y+h, x:x+w])
-            # print(data[y:y+h, x:x+w])
-            # 2: index 4
-            # a: index 13
-            # x, y, w, h = json_map[4]["bbox"]
-            # print(data[y:y+h, x:x+w])
-            image = np.where(image <= 100, 0, 255)
-            template = np.where(template <= 100, 0, 255)
-            # get the maximum of both sizes
-            max_size = (np.max([template.shape[0], image.shape[0]]), np.max([template.shape[1], image.shape[1]]))
-            # maps the smaller image onto bigger
-            centered_image = np.ones(max_size) * 255
-            centered_template = np.ones(max_size) * 255
-            image_min = np.subtract(max_size, image.shape) // 2
-            image_max = np.add(max_size, image.shape) // 2
-            template_min = np.subtract(max_size, template.shape) // 2
-            template_max = np.add(max_size, template.shape) // 2
-            # sets the values
-            centered_image[image_min[0]: image_max[0], image_min[1]: image_max[1]] = image
-            centered_template[template_min[0]:template_max[0], template_min[1]:template_max[1]] = template
-            # if(i == 53 or i == 54 or i == 95 or i == 143):
-            #     print(centered_image)
-            #     print("||||")
-            #     print(centered_template)
-            # releases unused memory
-            del image
+    # for i, char in enumerate(characters):
+    #     # characters[0][1][1:-1, 4:-5] #2
+    #     # characters[1][1][1:-1, 2:-4] #a
+    #     # characters[2][1][2:-2, 1:-3] #c
+    #     # characters[3][1][3:-2, 2:-5] #dot
+    #     # characters[4][1][1:-3, 1:-4] #e
+    #     print(f"Loop for {char[0]}: ")
+    #     template = np.array(char[1])
+    #     # curr_ncc_map = []
+    #     for j, item in enumerate(json_map):
+    #         x, y, w, h = item["bbox"]
+    #         image = np.array(data[y:y + h, x:x + w])
+    #         # if(j == 141):
+    #         # print(data[y:y+h, x:x+w])
+    #         # print(data[y:y+h, x:x+w])
+    #         # 2: index 4
+    #         # a: index 13
+    #         # x, y, w, h = json_map[4]["bbox"]
+    #         # print(data[y:y+h, x:x+w])
+    #         image = np.where(image <= 100, 0, 255)
+    #         template = np.where(template <= 100, 0, 255)
+    #         # get the maximum of both sizes
+    #         max_size = (np.max([template.shape[0], image.shape[0]]), np.max([template.shape[1], image.shape[1]]))
+    #         # maps the smaller image onto bigger
+    #         centered_image = np.ones(max_size) * 255
+    #         centered_template = np.ones(max_size) * 255
+    #         image_min = np.subtract(max_size, image.shape) // 2
+    #         image_max = np.add(max_size, image.shape) // 2
+    #         template_min = np.subtract(max_size, template.shape) // 2
+    #         template_max = np.add(max_size, template.shape) // 2
+    #         # sets the values
+    #         centered_image[image_min[0]: image_max[0], image_min[1]: image_max[1]] = image
+    #         centered_template[template_min[0]:template_max[0], template_min[1]:template_max[1]] = template
+    #         # if(i == 53 or i == 54 or i == 95 or i == 143):
+    #         #     print(centered_image)
+    #         #     print("||||")
+    #         #     print(centered_template)
+    #         # releases unused memory
+    #         del image
+    #
+    #         # NCC
+    #         numerator = np.sum(np.multiply(centered_image, centered_template))
+    #         denominator = np.sqrt(np.multiply(np.sum(np.square(centered_image)), np.sum(np.square(centered_template))))
+    #         if ((numerator / denominator) > 0.60):
+    #             # curr_ncc_map.append(j)
+    #             # print(j)
+    #             json_map[j]["name"] = str(char[0])
+    # return json_map
 
-            # NCC
-            numerator = np.sum(np.multiply(centered_image, centered_template))
-            denominator = np.sqrt(np.multiply(np.sum(np.square(centered_image)), np.sum(np.square(centered_template))))
-            if ((numerator / denominator) > 0.60):
-                # curr_ncc_map.append(j)
-                # print(j)
-                json_map[j]["name"] = str(char[0])
-    return json_map
-    # PMCC
-    # image_mean = np.mean(centered_image)
-    # template_mean = np.mean(centered_template)
-    # image_std = np.std(centered_image)
-    # template_std = np.std(centered_template)
-    # print(image_mean)
-    # print(image_std, "\n")
-    # summation = np.sum(np.multiply((centered_image - image_mean), (centered_template - template_mean)))
-    # if summation == 0 and (image_std == 0 or template_std == 0):
-    #     corr = 1000
-    # else:
-    #     corr = np.divide(summation, (np.std(centered_image) * np.std(centered_template)))
-    # corrs.append(corr)
-    # print(f"{i}: {corr}")
-    # mean_corr = np.mean(corrs)
-    # max_corr = np.max(corrs) * 0.9
-    # print(max_corr)
-    # for i, corr in enumerate(corrs):
-    #     if (corr > max_corr):
-    #         print(i)
-    # for irow in range(image.shape[0]):
-    #     for icol in range(image.shape[1]):
-    #         CCorr = 0
-    #         for trow in range(template.shape[0]):
-    #             for tcol in range(template.shape[1]):
-    #                 CCorr += abs(image[irow][icol]- template[trow][tcol])
-    # print(CCorr)
+    for i, img_kp in enumerate(image_kps):
+        for j, tmplt_kp in enumerate(template_kps):
+            goodmatch = 0
+            for m, kpi in enumerate(img_kp):
+                for n, kpt in enumerate(tmplt_kp):
+                    x1, y1 = kpi
+                    x2, y2 = kpt
+                    if (np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) < 4):
+                        goodmatch += 1
+            print(i, characters[j][0], goodmatch)
 
     # TODO
     # raise NotImplementedError
